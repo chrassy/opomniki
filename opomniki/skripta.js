a414c5f44b3787a66ad9de08cbe69239ad42e041
Aljaz Klanecek <aljaz.klanecek@gmail.com>
2016-03-11 10:27:04 +0000

    delujoča finalna verzija

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -9,10 +9,12 @@ window.addEventListener('load', function() {
 	}
 	
 	var dodajOpomnik = function() {
-		var naziv = document.querySelector("#naziv_opomnika").value
-		var cas = document.querySelector("#cas_opomnika").value
-		//opomnik.innerHTML += ""
-		//opomnik.removeChild(opomnik);
+		var naziv = document.querySelector("#naziv_opomnika").value;
+		var cas = document.querySelector("#cas_opomnika").value;
+	    document.querySelector("#naziv_opomnika"). value = "";
+		document.querySelector("#cas_opomnika").value = "";
+		document.querySelector("#opomniki").innerHTML += " \
+			<div class = 'opomnik rob senca'><div class='naziv_opomnika'>" + naziv + "</div> <div class='cas_opomnika'>Opomnik čez <span>" + cas + "</span> sekund.</div>";
 		
 	}
 	document.querySelector("#prijavniGumb").addEventListener("click", potrdiPrijavo);
@@ -31,6 +33,13 @@ window.addEventListener('load', function() {
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
+			if(cas == 0){
+				var naziv_opomnika = opomnik.querySelector(".naziv_opomnika").innerHTML;
+				alert("Opomnik!\n\nZadolžitev '" + naziv_opomnika + "' je potekla!");
+				document.querySelector("#opomniki").removeChild(opomnik);
+			}else{
+				casovnik.innerHTML = cas - 1;
+			}
 		}
 	}
 	setInterval(posodobiOpomnike, 1000);
